65cd8ca7ad3a289b2d22900e3ebdc432238b0225
cclshadow <cclynch@ualberta.ca>
2025-04-03 16:51:03 -0600

    BASIC TEST LOOP

--- RPI-0-2-W-CODE/OUR_START_SCRIPT.py
+++ RPI-0-2-W-CODE/OUR_START_SCRIPT.py
@@ -1,1 +1,8 @@
+import time
+from datetime import datetime
+
+while True:
+    current_time = datetime.now().strftime("%H:%M:%S")
+    print(f"Current time: {current_time}")
+    time.sleep(1)  # Wait for 1 second before next print
 
